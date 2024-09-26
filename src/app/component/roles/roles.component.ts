d7148bf2ce45c69673dbc7d295159573685747c1
Dakshsharma41 <sharmadaksh39@gmail.com>
2024-09-26 18:30:18 +0530

    dayfinal

--- src/app/component/roles/roles.component.ts
+++ src/app/component/roles/roles.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -8,19 +9,45 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
-firstName:string="Angular";
- angularVersion ="Version 18";
- version: number=18;
- isActive :boolean=false;
- currentDate:Date=new Date();
- inputType: string ="radio";
- selectedState: string ='';
+export class RolesComponent implements OnInit{
 
- showWelcomeAlert (){
-alert("Welome Bro")
- }
- showMessage(message:string){
-alert(message)
- }
+
+  roleList:any []=[];
+http = inject(HttpClient);
+
+ngOnInit(): void {
+    
+}
+
+getAllRoles(){
+  this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:any)=>{
+
+  })
+
+}
+
+
+
+
+
+
+
+
+
+
+// firstName:string="Angular";
+//  angularVersion ="Version 18";
+//  version: number=18;
+//  isActive :boolean=false;
+//  currentDate:Date=new Date();
+//  inputType: string ="radio";
+//  selectedState: string ='';
+
+//  showWelcomeAlert (){
+// alert("Welome Bro")
+//  }
+//  showMessage(message:string){
+// alert(message)
+//  }
+// 
 }
