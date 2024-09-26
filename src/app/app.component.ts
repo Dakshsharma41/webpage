b21dd80ddb12cfa7d5ae4ab6f5b0ed2831c89bf7
Dakshsharma41 <sharmadaksh39@gmail.com>
2024-09-26 17:47:15 +0530

    commmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { DesignationComponent } from './component/designation/designation.component';
+import { MasterComponent } from './component/master/master.component';
+import { RolesComponent } from './component/roles/roles.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DesignationComponent,MasterComponent,RolesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/component/roles/roles.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent {
+firstName:string="Angular";
+ angularVersion ="Version 18";
+ version: number=18;
+ isActive :boolean=false;
+ currentDate:Date=new Date();
+ inputType: string ="radio";
+ selectedState: string ='';
+
+ showWelcomeAlert (){
+alert("Welome Bro")
+ }
+ showMessage(message:string){
+alert(message)
+ }
+}
