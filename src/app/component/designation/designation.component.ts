66ab24a23357673ecab0ec59af0c5131f81a56fb
Dakshsharma41 <sharmadaksh39@gmail.com>
2024-09-27 16:41:54 +0530

    27work push

--- src/app/component/designation/designation.component.ts
+++ src/app/component/designation/designation.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component,inject,OnInit } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { APIResponseModel, IDesignation } from '../../model/class/interface/role';
 
 @Component({
   selector: 'app-designation',
@@ -7,6 +10,20 @@ import { Component } from '@angular/core';
   templateUrl: './designation.component.html',
   styleUrl: './designation.component.css'
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit{
 
+designationList: IDesignation[]=[];
+isLoader: boolean = true;
+
+  masterService =inject(MasterService)
+
+  ngOnInit(): void{
+    this.masterService.getDesignation().subscribe((result:APIResponseModel)=>{
+      this.designationList = result.data;
+      this.isLoader =false;
+    },error=>{
+      alert("API error")
+      this.isLoader =false;
+    })
+  }
 }
--- src/app/component/roles/roles.component.ts
+++ src/app/component/roles/roles.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { APIResponseModel, IRole } from '../../model/class/interface/role';
 
 @Component({
   selector: 'app-roles',
@@ -11,17 +12,20 @@ import { FormsModule } from '@angular/forms';
 })
 export class RolesComponent implements OnInit{
 
-
-  roleList:any []=[];
+  
+  roleList : IRole [] = [];
 http = inject(HttpClient);
+// constructor(private http: HttpClient){
+
+// }
 
 ngOnInit(): void {
-    
+    this.getAllRoles()
 }
 
 getAllRoles(){
-  this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:any)=>{
-
+  this.http.get<APIResponseModel>("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:APIResponseModel)=>{
+    this.roleList= res.data;
   })
 
 }
