f2129c8e5c3863dedef5480463e10bc2df8c134a
Dakshsharma41 <sharmadaksh39@gmail.com>
2024-09-30 11:58:28 +0530

    mm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { DesignationComponent } from './component/designation/designation.component';
 import { MasterComponent } from './component/master/master.component';
 import { RolesComponent } from './component/roles/roles.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,DesignationComponent,MasterComponent,RolesComponent],
+  imports: [RouterOutlet,DesignationComponent,MasterComponent,RolesComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
